7b6ddf1741615a21ded02ba5f302b4eb8d4468fd
Jagathsai Madhagani <103414774+jagathsai@users.noreply.github.com>
2023-06-09 17:38:25 +0530

    Update BaseTest.java

--- src/test/java/FlipkartTestingGrid/BaseTest.java
+++ src/test/java/FlipkartTestingGrid/BaseTest.java
@@ -3,27 +3,43 @@ package FlipkartTestingGrid;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class BaseTest {
     protected WebDriver driver;
 
+    public BaseTest() {
+        // Default constructor
+    }
+
     @BeforeTest
-    public void setUp() {     
+    public void setUp() {
+        // Default grid URL and browser capability
+        String gridUrlString = "http://192.168.1.5:4444/wd/hub";
+        String browserName = "chrome";
+
         URL gridUrl = null;
         try {
-            gridUrl = new URL("http://192.168.1.5:4444/wd/hub");
+            gridUrl = new URL(gridUrlString);
         } catch (MalformedURLException e) {
             e.printStackTrace();
-        }     
+        }
+
+        // Initialize WebDriver using the provided grid URL and browser capability
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName(browserName);
         ChromeOptions options = new ChromeOptions();
-        options.setCapability("browserName", "chrome");
+        options.merge(capabilities);
+
         driver = new RemoteWebDriver(gridUrl, options);
     }
+
 
     @AfterTest
     public void tearDown() {
